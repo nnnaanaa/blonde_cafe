a33bf02d1ad6c858fca18d98ea3a3944cbfbe914
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2024-01-03 14:00:18 +0900

    更新履歴をいい感じに

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 "use strict";
 
+/*-------------------------------------------
+ヘッダー
+-------------------------------------------*/
+// document.addEventListener("DOMContentLoaded", function() {
+//     const listItems = document.querySelectorAll(".rotate-word");
+//     let currentIndex = 0;
 
+//     function showTtlItem() {
+//         // 一つ前の要素からクラスを削除
+//         // 最初の要素から削除する場合は、最後の要素にクラスがないことを確認
+//         if (currentIndex > 0) {
+//           listItems[currentIndex - 1].classList.remove("show-ttl");
+//         } else {
+//           listItems[listItems.length - 1].classList.remove("show-ttl");
+//         }
+    
+//         // 現在の要素にクラスを追加
+//         listItems[currentIndex].classList.add("show-ttl");
+    
+//         // インデックスを更新し、10秒後に再度showTtlItemを呼び出す
+//         currentIndex = (currentIndex + 1) % listItems.length;
+//         setTimeout(showTtlItem, 3500); // 7秒ごとに表示が変わる
+//       }
+  
+//     // 初回呼び出し
+//     showTtlItem();
+// });
+
+/*-------------------------------------------
+メイン
+-------------------------------------------*/
